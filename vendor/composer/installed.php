615b53a3a33706d93cf0d08e0698e015eec2b41a
zainululum09 <shadow.mx350@gmail.com>
2025-07-17 12:50:44 +0700

    init

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'daf06f96d6af36fa4d780f1c536164c5fad5f308',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8e452f6ed5f28bcf830daa1c5c4f1e974964e8c8',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'daf06f96d6af36fa4d780f1c536164c5fad5f308',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8e452f6ed5f28bcf830daa1c5c4f1e974964e8c8',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
